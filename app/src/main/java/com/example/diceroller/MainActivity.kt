70a1e61e12839824b0d5872b2581c2a026d693cc
anreh <anurehka@gmail.com>
2019-10-23 12:14:56 +0800

    QA

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val randomInt = Random().nextInt(6) + 1
-    val randomInt2 = Random().nextInt(6) + 1
-    val randomInt3 = Random().nextInt(6) + 1
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +22,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        val resultText3: TextView = findViewById(R.id.result_text3)
-        resultText.text = "Dice Rolled!"
-        resultText.text = randomInt.toString()
-        resultText2.text = randomInt.toString()
-        resultText3.text = randomInt.toString()
+        val randomInt = Random().nextInt(6) + 1
+        Toast.makeText(
+            this, "button clicked",
+            Toast.LENGTH_SHORT
+        ).show()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val diceImage2: ImageView = findViewById(R.id.dice_image2)
+        val diceImage3: ImageView = findViewById(R.id.dice_image3)
+
+        diceImage.setImageResource(getDice())
+        diceImage2.setImageResource(getDice())
+        diceImage3.setImageResource(getDice())
+    }
+
+        private fun getDice():Int {
+            val randomInt = Random().nextInt(6) + 1
+         return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
 
 
